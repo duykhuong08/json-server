000cd1a02e17d1848d18d34a7f949002049189c4
duykhuong08 <93174275+duykhuong08@users.noreply.github.com>
2021-10-26 12:28:17 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+$host ="localhost";
+$usename ="root";
+$password = "";
+$database = "thietbi";
+
+$conn = mysqli_connect($host,$usename,$password,$database);
+mysqli_query($conn,"SET NAMES 'utf8'");
+
+?>
