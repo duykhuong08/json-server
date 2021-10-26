ba35c65c1f3c6d10fcd402089872acf33bc04665
duykhuong08 <93174275+duykhuong08@users.noreply.github.com>
2021-10-26 16:16:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
-echo"
-[
-   {
-      "id":"6",
-      "tensp":" OPPO Reno6 Pro 5G ",
-      "giasp":"9000000",
-      "hinhanhsp":"https:\/\/cdn.tgdd.vn\/Products\/Images\/42\/236187\/oppo-reno6-pro-blue-1-600x600.jpg",
-      "motasp":"\u0110\u1eb6C \u0110I\u1ec2M N\u1ed4I B\u1eacT\r\nThi\u1ebft k\u1ebf \u1ea5n t\u01b0\u1ee3ng, m\u00e0u s\u1eafc c\u00e1 t\u00ednh - Thi\u1ebft k\u1ebf tr\u00e0n vi\u1ec1n, m\u1ecfng nh\u1eb9 ch\u1ec9 182g\r\nT\u0103ng t\u1ed1c k\u1ebft n\u1ed1i, d\u1eabn \u0111\u1ea7u xu h\u01b0\u1edbng - Dimensity 900 5G (6 nm) m\u1ea1nh m\u1ebd, k\u1ebft n\u1ed1i 5G si\u00eau nhanh\r\nChuy\u00ean gia nhi\u1ebfp \u1ea3nh ch\u00e2n dung - B\u1ed9 3 camera l\u00ean t\u1edbi 64 MP, s\u1eafc n\u00e9t, \u1ea5n t\u01b0\u1ee3ng\r\nM\u00e0n h\u00ecnh l\u1edbn, s\u1eafc n\u00e9t v\u1edbi t\u1ed1c \u0111\u1ed9 l\u00e0m m\u1edbi cao - M\u00e0n h\u00ecnh AMOLED 6.43 inch Full HD, 90Hz\r\nDung l\u01b0\u1ee3ng pin l\u1edbn, c\u00f4ng ngh\u1ec7 s\u1ea1c nhanh hi\u1ec7n \u0111\u1ea1i - Vi\u00ean pin 4300mAh, s\u1ea1c nhanh 65W"
-   },
-   
-]
+echo" hello everyone
 ";
 
 ?>
